28a385c8e62248c81874076f229084281d6f1b18
Alejandro Delgado <alu0100767452@ull.edu.es>
2016-04-23 15:51:45 +0100

    Configuración del servidor. Línea de comandos incluida

--- Servidor/Servidor.pro
+++ Servidor/Servidor.pro
@@ -10,3 +10,6 @@ CONFIG -= app_bundle
 TEMPLATE = app
 
 SOURCES += main.cpp
+
+DISTFILES += \
+    config.ini
--- Servidor/main.cpp
+++ Servidor/main.cpp
@@ -1,8 +1,49 @@
+
 #include <QCoreApplication>
+#include <QSettings>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QCoreApplication::setOrganizationName("ChatOsO");
+    QCoreApplication::setOrganizationDomain("ChatOsO");
+    QCoreApplication::setApplicationName("ServerChatOsO");
+
+    QSettings settings;
+
+
+
+    int op;
+    std::string ip_option;
+    std::string port_option;
+
+    if(argc == 1){
+
+        ip_option = settings.value("Direccion", "127.0.0.1").toString().toStdString();
+        port_option = settings.value("Puerto", 3000).toString().toStdString();
+
+
+
+
+    }else{
+
+        while( (op = getopt(argc, argv, "s:p:01")) != -1){
+            switch (op){
+                case '0':
+                case '1': break;
+                case 's': ip_option = std::string(optarg); break;
+                case 'p': port_option = std::string(optarg); break;
+                case '?': break;
+                default: std::fprintf(stderr, "?? getopt devolvio codigo de error 0%o ??\n", op);
+
+            }
+        }
+    }
+
 
     return a.exec();
 }
