9d7b2ce83fff8b904bc073c4183bc10584c94aa0
Alberto <alu0100698893@ull.edu.es>
2016-04-26 13:19:40 +0100

    modificado archivo.prot añadido login user

--- Cliente/my_socket_cliente.cpp
+++ Cliente/my_socket_cliente.cpp
@@ -1,6 +1,27 @@
 #include "my_socket_cliente.h"
 
-My_Socket_Cliente::My_Socket_Cliente(QObject *parent) : QObject(parent)
+My_Socket_Cliente::My_Socket_Cliente(QString dir_server, quint16 port_server, QString dir_socket_client,QObject *parent) :
+
+    QObject(parent)
+{
+    sslSocket = new QSslSocket(this);//creamos el socket que vamos a usar
+    sslSocket->connectToHostEncrypted(dir_server, port_server);
+    connect(sslSocket, SIGNAL(encrypted()), this, SLOT(ready()));
+    connect(sslSocket, SIGNAL(readyRead()), this, SLOT(readyRead()));
+
+}
+
+void My_Socket_Cliente::ready()//solo para enviar mensajes al servidor(logearme, esperar mensajes,...)
+{
+    QByteArray data = sslSocket->readAll();
+    //TODO:procesar datos (deserializar)
+    //TODO2:preparar respuesta "dataOUT"
+    //sslSocket->write(/*TODO2*/);
+}
+
+void My_Socket_Cliente::readyRead()//para enviarle mensajes al servidor ya cuando escribimos
 {
 
 }
+
+
--- Cliente/my_socket_cliente.h
+++ Cliente/my_socket_cliente.h
@@ -2,16 +2,22 @@
 #define MY_SOCKET_CLIENTE_H
 
 #include <QObject>
+#include <QSslSocket>
 
 class My_Socket_Cliente : public QObject
 {
     Q_OBJECT
+private:
+    QSslSocket* sslSocket;//socket que vamos a utilizar
+
 public:
-    explicit My_Socket_Cliente(QObject *parent = 0);
+    explicit My_Socket_Cliente(QString dir_server, quint16 port_server, QString dir_socket_client, QObject *parent = 0);
+
 
 signals:
 
 public slots:
+    void ready();
 };
 
 #endif // MY_SOCKET_CLIENTE_H
